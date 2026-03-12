db0e69d0198e850d92e9a100a82f9ae72e00db6c
OriE95 <orixim123@gmail.com>
2026-03-12 13:02:27 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'rishit-v1';
+const FILES = [
+  './compound-interest.html',
+  './manifest.json',
+  './icon-192.svg',
+  './icon-512.svg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(ks =>
+      Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
